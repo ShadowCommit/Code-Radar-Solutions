2092ca9f6630b6e6bd090b7c4aff5721238ebd5a
ShadowCommit <rakshit0674.becse24@chitkara.edu.in>
2025-01-27 19:51:06 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char a;
+    scanf("%c",&a);
+    switch(a){
+        case 'a':
+        printf("Vowel");
+    break;
+
+    case 'z':
+        printf("Consonant");
+    break;
+
+    }
+    return 0;
+}
